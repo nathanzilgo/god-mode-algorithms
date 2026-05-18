c4f06d7e7fe517e60d4d54e9d3d50f3249373990
Nathan Fernandes Pedroza <nathan.pedroza@ccc.ufcg.edu.br>
2026-05-18 20:48:55 -0300

    feat: implement recursive algorithm to traverse and print indexed nested arrays

---
+++ nested_array.py
@@ -0,0 +1,51 @@
+# /*
+#       * In this problem you have a nested array of strings.
+#       * Lists of strings can be nested to an arbitrary depth.
+#       * Print each element contained in the list, including all elements in nested lists.
+#       * Each element should be printed on a new line with the element's index at the beginning of the line.
+#       *
+#       * Ex:
+#       * 0: 1
+#       * 1: 2
+#       * 2: 3
+#       * 3.0: blue
+#       * 3.1: yellow
+#       * 3.2: red
+#       * 4: 5
+#       * 5.2.0: biplane
+#       * etc...
+#       */
+
+nested_array = [
+    "1", "2", "3",
+    ["blue", "yellow", "red"],
+    "5",
+    [
+        "helicoptor", "blimp",
+        [
+            "biplane", "seaplane",
+            ["B-52", "F-35", "A-12"]
+        ]
+    ],
+    "7"
+]
+
+def solution():
+    for x in range(len(nested_array)):
+        if type(nested_array[x]) == list:
+            aux(nested_array[x], str(x))
+        else:
+            print(f"{x}: {nested_array[x]}")
+
+def aux(arr, prefix=""):
+    for i, val in enumerate(arr):
+        if type(val) == list:
+            aux(val, f"{prefix}.{i}")
+        else:
+            print(f"{prefix}.{i}: {val}")
+
+
+if __name__ == "__main__":
+    solution()
+
+
